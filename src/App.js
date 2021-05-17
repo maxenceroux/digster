85205350fef274e87fba7c21c7c2004f8c52b3ab
maxenceroux <maxence.b.roux@gmail.com>
2021-05-17 23:22:58 +0200

    Add infinite scroll

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { getTokenFromUrl } from "./spotify";
 import SpotifyWebApi from 'spotify-web-api-js';
 
@@ -8,13 +8,74 @@ import Login from "./Login";
 
 const spotifyApi = new SpotifyWebApi();
 
+// styling post container
+const divStyle = {
+  color: 'blue',
+  height: '250px',
+  textAlign: 'center',
+  padding: '5px 10px',
+  background: '#eee',
+  marginTop: '15px'
+};
+
+const containerStyle = {
+  maxWidth: '1280px',
+  margin: '0 auto',
+}
+
+const albumss = {
+  1: {
+    id: 1,
+    name: 'ABC'
+  },
+  3: {
+    id: 3,
+    name: 'DEF'
+  }
+}
+
+
 function App() {
   const [profile, setProfile] = useState();
   const [token, setToken] = useState();
   const [albums, setAlbums] = useState();
+  const [postList, setPostList] = useState({
+    list: [1, 2, 3, 4]
+  });
+  const [page, setPage] = useState(1);
+  const loader = useRef(null);
 
-  console.log("hq")
+  useEffect(() => {
+    var options = {
+      root: null,
+      rootMargin: "20px",
+      threshold: 1.0
+    };
+    // initialize IntersectionObserver
+    // and attaching to Load More div
+    const observer = new IntersectionObserver(handleObserver, options);
+    if (loader.current) {
+      observer.observe(loader.current)
+    }
 
+  }, []);
+
+  useEffect(() => {
+    // here we simulate adding new posts to List
+    const newList = postList.list.concat([page, 1, 1, 1]);
+    setPostList({
+      list: newList
+    })
+  }, [page])
+
+  // here we handle what happens when user scrolls to Load More div
+  // in this case we just update page variable
+  const handleObserver = (entities) => {
+    const target = entities[0];
+    if (target.isIntersecting) {
+      setPage((page) => page + 1)
+    }
+  }
 
   useEffect(() => {
     const hash = getTokenFromUrl();
@@ -37,14 +98,43 @@ function App() {
   }, []);
 
   console.log(token);
-  console.log(albums)
+
+  console.log(albums);
+  console.log(postList);
   return (
     <div className="App">
       <h1>Digster</h1>
       {profile ? <h1><Profile profile={profile} /> </h1> :
         <Login />}
-      <div className="albums"></div>
+      {albums ? <div>
+        {Object.keys(albums).map((value, index) => {
+          return <p> {albums[value]["album"]["name"]}</p>
+        })
+        }
+      </div> : <div />
+      }
+      <div className="container" style={containerStyle}>
+        <div className="loading" ref={loader}>
+          <h2>Load More</h2>
+        </div>
+
+        {/* <div className="post-list">
+          {
+            postList.list.map((post, index) => {
+              return (<div key={index}
+                className="post"
+                style={divStyle}>
+                <h2> {post} </h2>
+              </div>)
+            })
+          }
+          <div className="loading" ref={loader}>
+            <h2>Load More</h2>
+          </div>
+        </div> */}
+      </div>
     </div>
+
   );
 }
 
