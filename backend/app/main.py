03922d8f3d72ce0343502a425777bacf98faeb66
maxenceroux <maxence.b.roux@gmail.com>
2021-05-23 12:38:52 +0200

    Add background task management with celery

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,9 @@
 from typing import Optional
+from fastapi import Body, FastAPI
+from fastapi.responses import JSONResponse
+from celery.result import AsyncResult
 
-from fastapi import FastAPI
+from worker import create_task
 
 app = FastAPI()
 
@@ -13,3 +16,20 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.post("/tasks", status_code=201)
+def run_task():
+    task = create_task.delay()
+    return JSONResponse({"task_id": task.id})
+
+
+@app.get("/tasks/{task_id}")
+def get_status(task_id):
+    task_result = AsyncResult(task_id)
+    result = {
+        "task_id": task_id,
+        "task_status": task_result.status,
+        "task_result": task_result.result,
+    }
+    return JSONResponse(result)
---
+++ backend/app/worker.py
@@ -0,0 +1,17 @@
+import os
+import time
+
+from celery import Celery
+
+
+celery = Celery(__name__)
+celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "redis://redis:6379")
+celery.conf.result_backend = os.environ.get(
+    "CELERY_RESULT_BACKEND", "redis://redis:6379"
+)
+
+
+@celery.task(name="create_task")
+def create_task():
+    time.sleep(10)
+    return True
